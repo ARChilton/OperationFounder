ca2f3701ad89d08217888418ac081eaba529517b
ARChilton <adam@dsan.co.uk>
2017-02-13 14:19:18 -0500

    LOGIN PAGE

--- www/js/operationFounder.js
+++ www/js/operationFounder.js
@@ -1,3 +1,12 @@
+/*
+This is the javascript for the operationFounder app
+*/
+
+// global variables
+var baseCodes = ['opFounder123', 'adam', 'martin'];
+var base;
+var appdb;
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
@@ -5,7 +14,9 @@ function onDeviceReady() {
     console.log('deviceready');
     if (cordova.platformId == 'android') {
         StatusBar.backgroundColorByHexString("#283593"); //#333 grey #00796B is 700 color for teal
+
     }
+
 }
 document.addEventListener("pause", onPause, false);
 
@@ -24,11 +35,10 @@ function onResume() {
 
 }
 
-var baseCodes = ['opFounder123', 'adam', 'martin'];
-var base;
 
 ons.ready(function () {
     console.log('ons-ready function fired');
+
     // --- MENU CONTROLS ---
     // Opens the menu and adds the shadow on the right edge
     function openMenu() {
@@ -43,6 +53,17 @@ ons.ready(function () {
         openMenu();
     })
 
+    // login function
+    appdb = new PouchDB('opFounderAppDb');
+    appdb.get('login')
+        .catch(function (err) {
+            navi.bringPageTop('loginPage.html', {
+                animation: 'none'
+            });
+            base = false;
+        });
+
+
     //listens to onsenui event for the splitter menu closing line 22815 of onsenui.js
     //removes the shadow when the menu closes
     $('#menu').on('postclose', function () {
@@ -50,31 +71,106 @@ ons.ready(function () {
         $('#menu').removeClass('menuShadow');
     });
 
-    $('.loginButton').on('click', function () {
-        var baseCodeInput = $('#baseCode').val();
-        base = baseCodes.indexOf(baseCodeInput);
 
-        if (base > -1) {
-            //  good
-            if (base == 0) {
-                navi.bringPageTop('admin.html', {
-                    animation: 'none'
-                });
-            } else {
-                console.log('pass your base is ' + base);
-                navi.bringPageTop('page1.html', {
-                    animation: 'none'
-                });
-            }
-        } else {
-            //  bad
-            ons.notification.alert({
-                message: 'Please try re-entering your base code or contact the event organisers',
-                title: 'Incorrect Base Code',
-                cancelable: true
+    document.addEventListener('postpush', function (event) {
+        console.log('page pushed');
+        if ($('#loginPage').length) {
+            //loginPage.html
+            $('.loginButton').on('click', function () {
+                if ($('#baseCode').val != '' && $('#userName').val() != '') {
+                    var baseCodeInput = $('#baseCode').val();
+                    var name = $('#userName').val();
+                    base = baseCodes.indexOf(baseCodeInput);
+
+                    if (base > -1) {
+                        //  good
+                        if (base == 0) {
+                            navi.bringPageTop('admin.html', {
+                                animation: 'fade'
+                            });
+                            appdb.get('login').then(function (doc) {
+                                    appdb.put({
+                                        _id: doc._id,
+                                        _rev: doc._rev,
+                                        base: 'admin',
+                                        name: name
+                                    })
+                                })
+                                .catch(function (err) {
+                                    appdb.put({
+                                        _id: 'login',
+                                        base: 'admin',
+                                        name: name
+                                    }).then(function () {
+                                        ons.notification.alert({
+                                            title: 'Welcome ' + name,
+                                            message: 'Welcome to the Operation Founder admin app, here you can view all team scores as they come in.',
+                                            cancelable: true
+                                        });
+                                    }).catch(function () {
+                                        ons.notification.alert({
+                                            title: 'Error saving user',
+                                            message: 'You have logged in but there was an error saving your user credentials, the app will require you to log in again if you close it.',
+                                            cancelable: true
+                                        });
+                                    });
+
+
+                                });
+                        } else {
+                            console.log('pass your base is ' + base);
+                            navi.bringPageTop('page1.html', {
+                                animation: 'fade'
+                            });
+                            appdb.get('login').then(function (doc) {
+                                    appdb.put({
+                                        _id: doc._id,
+                                        _rev: doc._rev,
+                                        base: base,
+                                        name: name
+                                    })
+                                })
+                                .catch(function () {
+                                    appdb.put({
+                                        _id: 'login',
+                                        base: base,
+                                        name: name
+                                    }).then(function () {
+                                        ons.notification.alert({
+                                            title: 'Welcome ' + name,
+                                            message: 'Welcome to the Operation Founder base app, here you can enter team scores and log their details. This will instantaniously update in HQ but please do still write down on paper too.',
+                                            cancelable: true
+                                        });
+                                    }).catch(function () {
+                                        ons.notification.alert({
+                                            title: 'Error saving user',
+                                            message: 'You have logged in but there was an error saving your user credentials, the app will require you to log in again if you close it.',
+                                            cancelable: true
+                                        });
+                                    });
+
+                                });
+                        }
+                    } else {
+                        //  bad
+                        ons.notification.alert({
+                            message: 'Please try re-entering your base code or contact the event organisers',
+                            title: 'Incorrect Base Code',
+                            cancelable: true
+                        });
+                    }
+
+                } else {
+                    ons.notification.alert({
+                        message: 'Please enter both your name and your base code provided by the event organisers',
+                        title: 'Missing inputs',
+                        cancelable: true
+                    });
+
+                }
             });
-        }
-    });
+        } // end of loginPage.html
 
+    });
 
 });
