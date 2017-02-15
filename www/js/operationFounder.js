7f2c25294a4f08d5024f0d3c0b442d7f5cfadb87
ARChilton <adam@dsan.co.uk>
2017-02-14 19:15:13 -0500

    updates

--- www/js/operationFounder.js
+++ www/js/operationFounder.js
@@ -5,6 +5,8 @@ This is the javascript for the operationFounder app
 // global variables
 var baseCodes = ['opFounder123', 'adam', 'martin'];
 var appdb;
+var basedb;
+var remotedb;
 // login variables
 var base;
 var name;
@@ -44,26 +46,49 @@ function onResume() {
 
 }
 
+function updateExisting(patrolNo, timeIn, timeOut, wait, offRoute, totalScore, editable) {
+    $('#log-' + patrolNo).html("<td>" + patrolNo + "</td><td>" + timeIn + "</td><td>" + timeOut + "</td><td class='hide landscapeShow'>" + wait + "</td><td class='hide landscapeShow mdl-data-table__cell--non-numeric'>" + offRoute + "</td><td>" + totalScore + "</td>");
+}
+
+function updateTable(patrolNo, timeIn, timeOut, wait, offRoute, totalScore, editable) {
+    $('#logsTable').append("<tr id='log-" + patrolNo + "'><td>" + patrolNo + "</td><td>" + timeIn + "</td><td>" + timeOut + "</td><td class='hide landscapeShow'>" + wait + "</td><td class='hide landscapeShow mdl-data-table__cell--non-numeric'>" + offRoute + "</td><td>" + totalScore + "</td></tr>");
+}
+
+function updateTableFromDb(doc) {
+    for (var i = 0, l = doc.total_rows; i < l; i++) {
+        console.log(doc.rows[i].doc.patrol);
+        var path = doc.rows[i].doc;
+        updateTable(path.patrol, path.timeIn, path.timeOut, path.wait, path.offRoute, path.totalScore, path.editable);
+        orientationLandscapeUpdate();
+    }
+}
+
+function orientationLandscapeUpdate() {
+    if (ons.orientation.isLandscape()) {
+        $('.landscapeHide').addClass('hide');
+        $('.landscapeShow').removeClass('hide');
+    }
+}
+
+function orientationUpdates() {
+    if (ons.orientation.isLandscape()) {
+        $('.landscapeHide').addClass('hide');
+        $('.landscapeShow').removeClass('hide');
+    } else {
+        $('.landscapeShow').addClass('hide');
+        $('.landscapeHide').removeClass('hide');
+    }
+}
 
 ons.ready(function () {
     console.log('ons-ready function fired');
     //make every device and webpage android styled for familiarity
     ons.forcePlatformStyling('android');
     // if the user has their phone set landscape on starting
-    if (ons.orientation.isLandscape()) {
-        $('.landscapeHide').addClass('hide');
-        $('.landscapeShow').removeClass('hide');
-    }
+    orientationLandscapeUpdate();
     // orientation event listener
     ons.orientation.on('change', function () {
-        if (ons.orientation.isLandscape()) {
-
-            $('.landscapeHide').addClass('hide');
-            $('.landscapeShow').removeClass('hide');
-        } else {
-            $('.landscapeShow').addClass('hide');
-            $('.landscapeHide').removeClass('hide');
-        }
+        orientationUpdates();
     });
 
 
@@ -96,6 +121,29 @@ ons.ready(function () {
 
         });
 
+    basedb = new PouchDB('basedb');
+    remotedb = new PouchDB('http://ARChilton:2326@192.168.1.10:5984/founderhq');
+    basedb.sync(remotedb, {
+        live: true,
+        retry: true
+    }).on('change', function (change) {
+        // yo, something changed!
+        console.log(change);
+    }).on('paused', function (info) {
+        // replication was paused, usually because of a lost connection
+    }).on('active', function (info) {
+        // replication was resumed
+    }).on('error', function (err) {
+        // totally unhandled error (shouldn't happen)
+        console.log(err);
+    });
+    basedb.allDocs({
+            include_docs: true
+        })
+        .then(function (doc) {
+            updateTableFromDb(doc);
+        })
+        .catch();
 
     //listens to onsenui event for the splitter menu closing line 22815 of onsenui.js
     //removes the shadow when the menu closes
@@ -121,8 +169,121 @@ ons.ready(function () {
         sqTimeIn = $('#timeIn').val();
         sqTimeOut = $('#timeOut').val();
         sqWait = $('#wait').val();
-        sqOffRoute = $('#offRoute').val();
+        sqOffRoute = $('#offRoute').prop('checked');
         sqTotalScore = $('#total').val();
+        var missingInformationMessage = "";
+        if (sqPatrol == "") {
+            missingInformationMessage = '<p>Patrol number</p>';
+        }
+        if (sqTotalScore == "" && sqOffRoute == false) {
+            missingInformationMessage = missingInformationMessage + '<p>Total score for the patrol</p>';
+        }
+        if (missingInformationMessage != "") {
+            ons.notification.alert({
+                title: 'Missing fields',
+                messageHTML: '<p>Please fill in the following fields:</p>' + missingInformationMessage,
+                cancelable: true
+            });
+        } else {
+            if (sqTimeIn == "") {
+                var date = new Date();
+                sqTimeIn = date.toLocaleTimeString();
+            }
+            if (sqTimeOut == "") {
+                var date = new Date();
+                sqTimeOut = date.toLocaleTimeString();
+            }
+            if (sqWait == "") {
+                sqWait = 0;
+            }
+            if (sqTotalScore != "" && sqOffRoute) {
+                sqTotalScore = '';
+            }
+            var patrolRecord = {
+                _id: sqPatrol + ' base ' + base,
+                patrol: sqPatrol,
+                base: base,
+                timeIn: sqTimeIn,
+                timeOut: sqTimeOut,
+                timeWait: sqWait,
+                offRoute: sqOffRoute,
+                totalScore: sqTotalScore,
+                editable: true
+            }
+            basedb.get(patrolRecord._id)
+                .then(function (doc) {
+                    switch (doc.editable) {
+                        case true:
+                            ons.notification.confirm({
+                                title: 'Update',
+                                message: 'Are you sure you want to update patrol number ' + sqPatrol,
+                                cancelable: true
+                            }).then(function (input) {
+                                if (input == 1) {
+                                    basedb.put({
+                                        _id: doc._id,
+                                        _rev: doc._rev,
+                                        patrol: sqPatrol,
+                                        base: base,
+                                        timeIn: sqTimeIn,
+                                        timeOut: sqTimeOut,
+                                        timeWait: sqWait,
+                                        offRoute: sqOffRoute,
+                                        totalScore: sqTotalScore,
+                                        editable: true
+                                    }).then(function () {
+                                        updateExisting(sqPatrol, sqTimeIn, sqTimeOut, sqWait, sqOffRoute, sqTotalScore, true);
+                                    });
+                                }
+                            }).catch(function (err) {
+                                console.log(err);
+                            });
+                            break;
+                        case false:
+                            ons.notification.alert({
+                                title: 'No longer editable',
+                                message: 'This record has been updated by HQ and cannot be edited',
+                                cancelable: true
+                            });
+                            break;
+                    }
+
+
+                }).catch(function (err) {
+                    if (err.status == 404) {
+                        console.log('404 no prior record putting a new record');
+                        updateTable(sqPatrol, sqTimeIn, sqTimeOut, sqWait, sqOffRoute, sqTotalScore, true);
+                        return basedb.put(patrolRecord);
+
+                    } else if (err.status == 409) {
+                        switch (doc.editable) {
+                            case true:
+                                console.log('409 putting anyway');
+                                basedb.put({
+                                    _id: doc._id,
+                                    _rev: doc._rev,
+                                    base: base,
+                                    timeIn: sqTimeIn,
+                                    timeOut: sqTimeOut,
+                                    timeWait: sqWait,
+                                    offRoute: sqOffRoute,
+                                    totalScore: sqTotalScore,
+                                    editable: true
+                                });
+                                break;
+                            case false:
+                                console.log('409 alert message');
+                                ons.notification.alert({
+                                    title: 'No longer editable',
+                                    message: 'This record has been recorded by HQ and cannot be edited, please contact HQ to unlock',
+                                    cancelable: true
+                                });
+                                break;
+                        }
+                    }
+                });
+
+        }
     });
 
     // page change code
