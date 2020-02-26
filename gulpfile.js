059d9ab5a02de4b5e2ab9b15cf05def69dca084d
Adam Chilton <archilton.dsan@gmail.com>
2020-02-26 12:48:38 +0000

    got the build working again

--- gulpfile.js
+++ gulpfile.js
@@ -1,125 +1,133 @@
 /*eslint-env node*/
-const gulp = require('gulp');
-const runSequence = require('run-sequence');
-const del = require('del');
-const swPrecache = require('sw-precache');
-const path = require('path');
-const uglify = require('gulp-uglify-es').default;
-const pump = require('pump');
-const cleanCSS = require('gulp-clean-css');
-const htmlmin = require('gulp-htmlmin');
-const svgmin = require('gulp-svgmin');
-const autoprefixer = require('gulp-autoprefixer');
-const sitemap = require('gulp-sitemap');
+const { series, src, dest } = require("gulp");
+// const runSequence = require("run-sequence");
+const del = require("del");
+const swPrecache = require("sw-precache");
+const path = require("path");
+const uglify = require("gulp-uglify-es").default;
+const pump = require("pump");
+const cleanCSS = require("gulp-clean-css");
+const htmlmin = require("gulp-htmlmin");
+const svgmin = require("gulp-svgmin");
+const autoprefixer = require("gulp-autoprefixer");
+const sitemap = require("gulp-sitemap");
 
-const buildFolder = 'dist';
+const buildFolder = "dist";
 
-gulp.task('build', (cb) => {
-  runSequence(
-    'build-clean',
-    'minify-js',
-    'minify-css',
-    'minify-svg',
-    'minify-html',
-    'move-static',
-    'move-license',
-    'delete-monaca-components',
-    'sitemap',
-    'generate-service-worker',
-    'minify-service-worker',
-    cb
-  );
-});
+const buildClean = () => del([buildFolder]);
 
-gulp.task('build-clean', () => del([buildFolder]));
-
-gulp.task('generate-service-worker', (callback) => {
-
+const generateServiceWorker = callback => {
   const rootDir = buildFolder;
 
-  swPrecache.write(path.join(rootDir, 'sw.js'), {
-    staticFileGlobs: [rootDir + '/**/!(404*).{js,html,css,png,svg,jpg,gif,woff2,woff,ttf,eot,otf}'],
-    // staticFileGlobs: [rootDir + '*/*!(404**)/**/*.{js,html,css,png,svg,jpg,gif,woff2,woff,ttf,eot,otf}'],
-    stripPrefix: rootDir
-  }, callback);
-});
-
-gulp.task('minify-service-worker', (cb) => {
-  pump(
-    [
-      gulp.src(`${buildFolder}/sw.js`),
-      uglify(),
-      gulp.dest(buildFolder)
-    ],
-    cb
-  );
-});
-
-
-gulp.task('minify-js', (cb) => {
-  pump(
-    [
-      gulp.src('www/**/*.js'),
-      uglify({ compress: { drop_console: true } }),
-      gulp.dest(buildFolder)
-    ],
-    cb
-  );
-});
-
-gulp.task('minify-css', () => gulp.src('www/**/*.css')
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(cleanCSS({
-    compatibility: 'ie8',
-    level: {
-      1: {
-        all: true,
-        normalizeUrls: true
-      }
+  swPrecache.write(
+    path.join(rootDir, "sw.js"),
+    {
+      staticFileGlobs: [
+        rootDir +
+          "/**/!(404*).{js,html,css,png,svg,jpg,gif,woff2,woff,ttf,eot,otf}"
+      ],
+      // staticFileGlobs: [rootDir + '*/*!(404**)/**/*.{js,html,css,png,svg,jpg,gif,woff2,woff,ttf,eot,otf}'],
+      stripPrefix: rootDir
     },
-    rebase: false
+    callback
+  );
+};
 
-  }))
-  .pipe(gulp.dest(buildFolder)));
+const minifyServiceWorker = cb => {
+  pump([src(`${buildFolder}/sw.js`), uglify(), dest(buildFolder)], cb);
+};
 
-gulp.task('minify-html', () => gulp.src('www/**/*.html')
-  .pipe(htmlmin({
-    collapseWhitespace: true,
-    minifyJS: true,
-    removeComments: true
-  }))
-  .pipe(gulp.dest(buildFolder)));
+const minifyJs = cb => {
+  pump(
+    [
+      src("www/**/*.js"),
+      uglify({ compress: { drop_console: true } }),
+      dest(buildFolder)
+    ],
+    cb
+  );
+};
 
-gulp.task('minify-svg', () => gulp.src('www/**/*.svg')
-  .pipe(svgmin())
-  .pipe(gulp.dest(buildFolder)));
+const minifyCSS = () =>
+  src("www/**/*.css")
+    .pipe(
+      autoprefixer({
+        browserList: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(
+      cleanCSS({
+        compatibility: "ie8",
+        level: {
+          1: {
+            all: true,
+            normalizeUrls: true
+          }
+        },
+        rebase: false
+      })
+    )
+    .pipe(dest(buildFolder));
 
-gulp.task('move-static', () => gulp.src('www/**/*.{png,jpeg,gif,woff2,woff,ttf,eot,otf,xml,webmanifest,ico,md,txt}')
-  .pipe(gulp.dest(buildFolder)));
+const minifyHtml = () =>
+  src("www/**/*.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        minifyJS: true,
+        removeComments: true
+      })
+    )
+    .pipe(dest(buildFolder));
 
-gulp.task('minify-onsenui-css', () => gulp.src('www/lib/onsenui/css/*.css')
-  .pipe(cleanCSS({
-    level: {
-      1: {
-        all: true,
-        normalizeUrls: false
-      }
-    }
-  }))
-  .pipe(gulp.dest(`${buildFolder}/lib/onsenui/css`)));
+const minifySvg = () =>
+  src("www/**/*.svg")
+    .pipe(svgmin())
+    .pipe(dest(buildFolder));
+const moveStatic = () =>
+  src(
+    "www/**/*.{png,jpeg,gif,woff2,woff,ttf,eot,otf,xml,webmanifest,ico,md,txt}"
+  ).pipe(dest(buildFolder));
 
-gulp.task('delete-monaca-components', () => del([`${buildFolder}/components`]));
+const minifyOnsenuiCss = () =>
+  src("www/lib/onsenui/css/*.css")
+    .pipe(
+      cleanCSS({
+        level: {
+          1: {
+            all: true,
+            normalizeUrls: false
+          }
+        }
+      })
+    )
+    .pipe(dest(`${buildFolder}/lib/onsenui/css`));
+const deleteMonacaComponents = () => del([`${buildFolder}/components`]);
 
-gulp.task('move-license', () => {
-  gulp.src('license.txt')
-    .pipe(gulp.dest(buildFolder));
-});
+const moveLicense = () => {
+  src("license.txt").pipe(dest(buildFolder));
+};
 
-gulp.task('sitemap', () => gulp.src('www/**/*.html', { read: false })
-  .pipe(sitemap({
-    siteUrl: 'https://checkpointlive.com'
-  }))
-  .pipe(gulp.dest(buildFolder)));
+const sitemapGenerator = () =>
+  src("www/**/*.html", { read: false })
+    .pipe(
+      sitemap({
+        siteUrl: "https://checkpointlive.com"
+      })
+    )
+    .pipe(dest(buildFolder));
+
+exports.build = series(
+  buildClean,
+  minifyJs,
+  minifyCSS,
+  minifySvg,
+  minifyHtml,
+  moveStatic,
+  moveLicense,
+  deleteMonacaComponents,
+  sitemapGenerator,
+  generateServiceWorker,
+  minifyServiceWorker
+);
